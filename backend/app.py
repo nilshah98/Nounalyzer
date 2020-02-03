cd037e343f498535c3be8d94e80668e9cf56640b
nilshah98 <neel12@somaiya.edu>
2020-02-04 01:14:17 +0530

    sbasic d3 animation added

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,14 @@ CORS(app)
 def login(): 
    rssLink = request.form['rssLink'] 
    data = feedparser.parse(rssLink)
-   print(data['feed'])
    # refer - https://www.pythonforbeginners.com/feedparser/using-feedparser-in-python to use feedparser
    # pass data to NLP processing
-   return data.feed.subtitle
+   # processing the length of content in each entry in RSS
+   analysis = []
+   for entry in data["entries"]:
+      analysis.append(len(entry["summary"]))
+   print(analysis)
+   return { "data": analysis}
   
 if __name__ == '__main__': 
    app.run(debug = True) 
--- frontend/script.js
+++ frontend/script.js
@@ -8,12 +8,63 @@ rss.addEventListener("submit", (e) => {
     getData(rssName.value);
 })
 
+const scaleVals = (vals) => {
+  const scaleMin = 0;
+  const scaleMax = 500;
+
+  const valMin = Math.min(...vals);
+  const valMax = Math.max(...vals);
+
+  const percent = vals.map((val) => (val - valMin) / (valMax - valMin));
+  console.log(percent);
+
+  const scaled = percent.map((p) => (p * (scaleMax - scaleMin)) + scaleMin);
+
+  return scaled;
+
+}
+
 const getData = (feedURL) => {
     $.post("http://localhost:5000/rss",
     {
       rssLink: feedURL,
     },
-    function(data, status){
-      rssRes.textContent = `Data: ${data} Status: ${status}`;
+    function(res, status){
+      rssRes.textContent = `Data: ${res} Status: ${status}`;
+
+      const dataset = scaleVals(res.data);
+
+      console.log(dataset);
+
+      const svgHeight = 600, svgWidth = 600, barPadding = 5;
+      const barWidth = svgWidth / dataset.length;
+
+      d3.selectAll('svg > *').remove();
+
+      var svg = d3.select('svg')
+        .attr('width', svgWidth)
+        .attr('height', svgHeight)
+
+      var barChart = svg.selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('rect')
+        .attr('y', (d) => svgHeight)
+        .attr('height', (d) => d)
+        .attr('width', barWidth - barPadding)
+        .attr('class', 'bar')
+        .attr('transform', (d,i) =>{
+          const translate = [barWidth * i];
+          return `translate(${translate})`;
+        })
+        .transition()
+        .duration(300)
+        .attr('transform', (d,i) => {
+          const translate = [barWidth * i, -d];
+          return `translate(${translate})`;
+        })
+
+      
+
     });
 }
